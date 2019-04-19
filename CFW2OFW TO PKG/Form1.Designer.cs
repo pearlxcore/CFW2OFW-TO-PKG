b3f7fd1e8ef5bacaf2500bd30442bb8993ec15cc
pearlxcore <36906814+pearlxcore@users.noreply.github.com>
2019-04-20 05:11:26 +0800

    update program

--- CFW2OFW TO PKG/Form1.Designer.cs
+++ CFW2OFW TO PKG/Form1.Designer.cs
@@ -35,6 +35,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtGame
@@ -44,17 +51,17 @@
             this.txtGame.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtGame.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtGame.ForeColor = System.Drawing.SystemColors.Window;
-            this.txtGame.Location = new System.Drawing.Point(18, 72);
+            this.txtGame.Location = new System.Drawing.Point(20, 90);
             this.txtGame.Multiline = true;
             this.txtGame.Name = "txtGame";
             this.txtGame.ReadOnly = true;
-            this.txtGame.Size = new System.Drawing.Size(403, 20);
+            this.txtGame.Size = new System.Drawing.Size(471, 20);
             this.txtGame.TabIndex = 0;
             this.txtGame.TextChanged += new System.EventHandler(this.txtGame_TextChanged);
             // 
             // txtFolderPath
             // 
-            this.txtFolderPath.Location = new System.Drawing.Point(150, 72);
+            this.txtFolderPath.Location = new System.Drawing.Point(167, 90);
             this.txtFolderPath.Name = "txtFolderPath";
             this.txtFolderPath.Size = new System.Drawing.Size(100, 20);
             this.txtFolderPath.TabIndex = 4;
@@ -65,7 +72,7 @@
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(314, 103);
+            this.label1.Location = new System.Drawing.Point(390, 132);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(107, 16);
             this.label1.TabIndex = 3;
@@ -75,13 +82,13 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Font = new System.Drawing.Font("Verdana", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Verdana", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(14, 15);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(295, 32);
+            this.label2.Size = new System.Drawing.Size(451, 29);
             this.label2.TabIndex = 5;
-            this.label2.Text = "CFW2OFW TO PKG";
+            this.label2.Text = "CFW2OFW TO PKG (DEBUG PKG)";
             // 
             // label4
             // 
@@ -89,7 +96,7 @@
             this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label4.Font = new System.Drawing.Font("Verdana", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(15, 51);
+            this.label4.Location = new System.Drawing.Point(17, 58);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(200, 18);
             this.label4.TabIndex = 7;
@@ -98,7 +105,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(18, 98);
+            this.button1.Location = new System.Drawing.Point(20, 122);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 26);
             this.button1.TabIndex = 11;
@@ -106,26 +113,70 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(485, 135);
+            this.panel1.TabIndex = 12;
+            this.panel1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, -6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(460, 138);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(1, 111);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(482, 15);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(485, 138);
+            this.panel2.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(440, 142);
+            this.ClientSize = new System.Drawing.Size(510, 162);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtGame);
             this.Controls.Add(this.txtFolderPath);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CFW2OFW TO PKG";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,6 +190,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- CFW2OFW TO PKG/Form1.cs
+++ CFW2OFW TO PKG/Form1.cs
@@ -9,14 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Reflection;
-
+using System.Diagnostics;
 
 namespace CFW2OFW_TO_PKG
 {
 
     public partial class Form1 : Form
     {
-
+        string directory;
+        string[] files;
+        private int result;
         private static void extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
         {
             Assembly assembly = Assembly.GetCallingAssembly();
@@ -44,11 +46,12 @@ namespace CFW2OFW_TO_PKG
         public Form1()
         {
             InitializeComponent();
-
+            CheckForIllegalCrossThreadCalls = false;
             //this.txtFolderPath.Location = new System.Drawing.Point(20, 30);
             //this.txtFolderPath.Size = new System.Drawing.Size(200, 20);
             //this.Controls.Add(this.txtFolderPath);
-
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.MarqueeAnimationSpeed = 1;
             this.txtFolderPath.AllowDrop = true;
             this.txtFolderPath.DragEnter += new DragEventHandler(Form1_DragEnter);
             this.txtFolderPath.DragDrop += new DragEventHandler(Form1_DragDrop);
@@ -73,44 +76,20 @@ namespace CFW2OFW_TO_PKG
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                files = (string[])e.Data.GetData(DataFormats.FileDrop);
 
                 if (Directory.Exists(files[0]))
                 {
-                    string path = Environment.CurrentDirectory;
-                    extract("CFW2OFW_TO_PKG", path, "resources", "cygz.dll");
-                    extract("CFW2OFW_TO_PKG", path, "resources", "libeay32.dll");
-                    extract("CFW2OFW_TO_PKG", path, "resources", "MSCOMCTL.OCX");
-                    extract("CFW2OFW_TO_PKG", path, "resources", "mswinsck.ocx");
-                    extract("CFW2OFW_TO_PKG", path, "resources", "psn_package_npdrm.exe");
-                    extract("CFW2OFW_TO_PKG", path, "resources", "zlib1.dll");
+                    panel1.Visible = true;
+                    panel2.Visible = false;
+                    label1.Visible = false;
+                    
+                    backgroundWorker1.RunWorkerAsync();
 
-                    this.txtGame.Text = files[0];
-                    string dir = this.txtGame.Text;
-                    DirectoryInfo dir_info = new DirectoryInfo(dir);
-                    string directory = dir_info.Name;  // System32
-                    MessageBox.Show(directory, "GAME ID");
-
-                    string bat = "@echo off\npsn_package_npdrm.exe -n " + directory + ".CONF " + directory;
-                    System.IO.File.WriteAllText("go.bat", bat);
-
-                    string conf = "ContentID        = UP0001-" + directory + "_00-1111222233334444\nKlicensee        = 0x00000000000000000000000000000000\nDRMType          = Free\nContentType      = GameExec\nPackageVersion   = 01.00";
-                    System.IO.File.WriteAllText(directory + ".CONF", conf);
-
-                    //System.Diagnostics.Process.Start("go.bat");
-                    var process = System.Diagnostics.Process.Start("go.bat");
-                    process.WaitForExit();
-                    System.IO.File.Delete("go.bat");
-                    System.IO.File.Delete(directory + ".CONF");
-                    System.IO.File.Delete("cygz.dll");
-                    System.IO.File.Delete("libeay32.dll");
-                    System.IO.File.Delete("MSCOMCTL.OCX");
-                    System.IO.File.Delete("mswinsck.ocx");
-                    System.IO.File.Delete("psn_package_npdrm.exe");
-                    System.IO.File.Delete("zlib1.dll");
-
-                    MessageBox.Show("Done!", "Info");
-                    txtGame.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Only drag and drop game folder goddamitt!", "CFW2OFW TO PKG", MessageBoxButtons.OK, MessageBoxIcon.Hand);
                 }
             }
         }
@@ -129,7 +108,85 @@ namespace CFW2OFW_TO_PKG
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Set Game folder (to be converted) and this program in the same directory.\n\nDrag and Drop the game folder anywhere into this program.", "CFW2OFW TO PKG");
+            MessageBox.Show("Set Game folder (to be converted) and this program in the same directory.\n\nDrag and Drop the game folder anywhere into this program.\n\nIt will produce the PKG version of the game. The PKG can be installed on CFW or HAN PS3.\n\nBefore installing on HAN PS3, please enable debug PKG.", "CFW2OFW TO PKG", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
         }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string path = Environment.CurrentDirectory;
+            extract("CFW2OFW_TO_PKG", path, "resources", "cygz.dll");
+            extract("CFW2OFW_TO_PKG", path, "resources", "libeay32.dll");
+            extract("CFW2OFW_TO_PKG", path, "resources", "MSCOMCTL.OCX");
+            extract("CFW2OFW_TO_PKG", path, "resources", "mswinsck.ocx");
+            extract("CFW2OFW_TO_PKG", path, "resources", "psn_package_npdrm.exe");
+            extract("CFW2OFW_TO_PKG", path, "resources", "zlib1.dll");
+
+            this.txtGame.Text = files[0];
+            string dir = this.txtGame.Text;
+            DirectoryInfo dir_info = new DirectoryInfo(dir);
+            directory = dir_info.Name;  // System32
+
+            string bat = "@echo off\npsn_package_npdrm.exe -n " + directory + ".CONF " + directory;
+            System.IO.File.WriteAllText("go.bat", bat);
+
+            string conf = "ContentID        = UP0001-" + directory + "_00-1111222233334444\nKlicensee        = 0x00000000000000000000000000000000\nDRMType          = Free\nContentType      = GameExec\nPackageVersion   = 01.00";
+            System.IO.File.WriteAllText(directory + ".CONF", conf);
+
+            Process p = new Process();
+            p.StartInfo.FileName = "go.bat";
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            p.Start();
+
+            this.Text += " | Converting " + directory.ToString();
+            this.Refresh();
+
+
+
+            //System.Diagnostics.Process.Start("go.bat");
+
+            p.WaitForExit();
+            result = p.ExitCode;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = true;
+            label1.Visible = true;
+
+            this.Text = "CFW2OFW TO PKG";
+            this.Refresh();
+            if (result == 0) // success
+            {
+                delete();
+
+                MessageBox.Show("Operation completed successfully", "CFW2OFW TO PKG", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                delete();
+
+                MessageBox.Show("Operation failed", "CFW2OFW TO PKG", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+
+            }
+
+            txtGame.Clear();
+        }
+
+        void delete()
+        {
+            System.IO.File.Delete("go.bat");
+            System.IO.File.Delete(directory + ".CONF");
+            System.IO.File.Delete("cygz.dll");
+            System.IO.File.Delete("libeay32.dll");
+            System.IO.File.Delete("MSCOMCTL.OCX");
+            System.IO.File.Delete("mswinsck.ocx");
+            System.IO.File.Delete("psn_package_npdrm.exe");
+            System.IO.File.Delete("zlib1.dll");
+        }
+
     }
 }
